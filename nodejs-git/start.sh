f8666793daa66e5884ee6ab6efc887ed61b95e3e
vagrant <vagrant@spaasvm>
2014-03-19 18:54:13 +0000

    added building blocks of node containers (needs a bit more work)

---
+++ nodejs-git/start.sh
@@ -0,0 +1,27 @@
+export SERVICE_NAME=$1
+export GIT_URL=$2
+export SERVICE_PORT=$3
+export EXPOSE_PORT=$4
+
+
+if [ -z "$4" ]
+then
+ echo "USEAGE: $0 <SERVICENAME> <GIT_URL> <SERVICE_PORT> <EXPOSE_PORT>"
+ echo "EXAMPLE: $0 simplenode https://github.com/fuzzy-logic/simplenode.git 8888 4444"
+ exit 1
+fi
+
+EXISTING_IMAGE=`sudo docker images | grep $SERVICE_NAME | awk '{print $1}'`
+
+echo EXISTING_IMAGE: $EXISTING_IMAGE
+
+
+if [ -z "$EXISTING_IMAGE" ]
+then
+  cat Dockerfile | envsubst 
+  echo "RUNNING: cat Dockerfile | envsubst | sudo docker build -t $SERVICE_NAME . "
+  sudo cat Dockerfile | envsubst | docker build -t $SERVICE_NAME . 
+fi
+
+
+sudo docker run -p 127.0.0.1:$EXPOSE_PORT:$SERVICE_PORT $SERVICE_NAME
