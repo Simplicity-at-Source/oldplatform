92d23e7628064334dec0009628f6da695e76a788
David Dawson <david.dawson@simplicityitself.com>
2014-04-19 11:44:07 +0100

    Rename to cell-expressor, as it is one of many.  Add data-stores skeleton

---
+++ data-stores/riak/riak-expressor/container/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -jar /sp-control-plane-0.1.jar
---
+++ data-stores/riak/riak-node/service/src/test/groovy/simplepaas/controlplane/DestroyContainerSpec.groovy
@@ -0,0 +1,26 @@
+package simplepaas.controlplane
+
+import groovy.json.JsonBuilder
+import simplepaas.controlplane.commands.DestroyContainer
+import spock.lang.Specification
+
+class DestroyContainerSpec extends Specification {
+
+  JSONApi api = Mock(JSONApi)
+
+  def "Correctly calls the docker API"() {
+    given:
+    def command = new DestroyContainer(dockerApi: api)
+
+    when:
+    command("THECONTAINERID")
+
+    then:
+    1 * api.post("/containers/THECONTAINERID/kill")
+    1 * api.delete("/containers/THECONTAINERID")
+  }
+
+  def json(map) {
+    new JsonBuilder(map).toString()
+  }
+}
