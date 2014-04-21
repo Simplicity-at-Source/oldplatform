9a33db621537e0694fc1f0e1a114354c6ee980fd
fuzzy-logic <spam.gawain@gmail.com>
2014-04-21 04:45:14 +0000

    fixed test/demo script to show how proxy works

--- test-scripts/test-gene-store.sh
+++ test-scripts/test-gene-store.sh
@@ -5,5 +5,5 @@ echo ******************************************************************
 echo Testing proxy to control-plane...
 echo ******************************************************************
 echo
-echo "curl -vvv -X GET http://localhost:8080/ -H 'Host: sp-gene-store'"
-curl -X GET http://localhost:8080/ -H 'Host: sp-gene-store' | json_pp
+#curl -X GET http://localhost:8080/ -H 'Host: sp-gene_store' | json_pp
+curl -X GET http://localhost:8888/spapi/sp-gene_store/container | json_pp
--- test-scripts/test-phenotype-monitor.sh
+++ test-scripts/test-phenotype-monitor.sh
@@ -6,4 +6,5 @@ echo Testing proxy to sp-phenotype-monitor...
 echo ******************************************************************
 echo
 echo "curl -vvv -X GET http://localhost:8080/container -H 'Host: sp-phenotype-monitor'"
-curl -X GET http://localhost:8080/ -H 'Host: sp-phenotype-monitor' | json_pp
+#curl -X GET http://localhost:8080/ -H 'Host: sp-phenotype-monitor' | json_pp
+curl -X GET http://localhost:8888/spapi/sp-phenotype_monitor/container | json_pp
