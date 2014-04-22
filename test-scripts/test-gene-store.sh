243c0e3da59bb077ee366d6fd2932c859a45c12b
fuzzy-logic <spam.gawain@gmail.com>
2014-04-22 15:12:58 +0000

    uber-any base image working

--- test-scripts/test-gene-store.sh
+++ test-scripts/test-gene-store.sh
@@ -6,4 +6,15 @@ echo Testing proxy to control-plane...
 echo ******************************************************************
 echo
 #curl -X GET http://localhost:8080/ -H 'Host: sp-gene_store' | json_pp
-curl -X GET http://localhost:8888/spapi/sp-gene_store/container | json_pp
+
+
+
+SERVICE_JSON='{ "id": "simplenode", "image": "sp_platform/uber-any", "env": { "GIT_REPO_URL": "https://github.com/fuzzy-logic/simplenode.git" } }'
+#curl -X GET http://localhost:8888/spapi/config/cell
+
+#curl -X POST http://localhost:8888/spapi/spapi/config/cell  -d "$SERVICE_JSON"
+echo curl -vvv -X POST http://172.17.0.3:8080/cell  -d \'$SERVICE_JSON\'
+#curl -vvv -X POST http://172.17.0.3:8080/cell  -d "$SERVICE_JSON"
+
+
+echo curl -vvv -X GET http://172.17.0.3:8080/
