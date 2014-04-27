3aa3c11fe59b169d083e4fc7f6ad2c1e059aa512
David Dawson <david.dawson@simplicityitself.com>
2014-04-27 11:28:11 +0100

    Stuff

---
+++ cell-layer/control-plane/service/src/main/groovy/simplepaas/controlplane/commands/CreateImage.groovy
@@ -0,0 +1,68 @@
+package simplepaas.controlplane.commands
+
+import groovy.json.JsonBuilder
+import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
+import simplepaas.controlplane.JSONApi
+
+@Slf4j
+class CreateImage {
+  @Autowired
+  JSONApi dockerApi
+  def proxyHostPort = 8888;
+
+  def call(json) {
+
+    def request = [
+            "Image": json.imageId,
+            "Env": getEnvironmentVariables(json)
+    ]
+
+    //ensure the image is downloaded
+    def imageUrl = "/images/create?fromImage=${json.imageId}"
+    log.info "Ensuring the image is downloaded : $imageUrl"
+    dockerApi.post(imageUrl)
+
+    def url = "/containers/create?name=${json.name}"
+    log.info "Requested to create a new container \"/containers/create?fromImage=${json.imageId}&name=${json.name}\" $request"
+
+    def dockerRet = dockerApi.post(url, new JsonBuilder(request).toString())
+
+    // If container is sp_proxy then bind port 8080 to host
+    if (json.imageId.contains("sp_proxy") || json.name.contains("sp_proxy")) {
+      def proxyStartJson = """
+            {
+                "PortBindings": { "8888/tcp": [{ "HostPort": "8888" }] },
+                "Privileged": false,
+                "PublishAllPorts": false
+           }
+            """
+      dockerApi.post("/containers/${dockerRet.Id}/start", proxyStartJson)
+    } else {
+      dockerApi.post("/containers/${dockerRet.Id}/start", new JsonBuilder([:]).toString())
+    }
+
+
+    [message: "Container created",
+            id: dockerRet.Id
+    ]
+  }
+
+  List getEnvironmentVariables(json) {
+    def env = json?.dependencies?.collect {
+      ["${it.dependency}_PORT=${it.port}", "${it.dependency}_HOST=${it.host}"]
+    }?.flatten()
+
+    if (!env) {
+      env = []
+    }
+
+    if (json.env) {
+      json.env.each { key, value ->
+        env << "$key=$value"
+      }
+    }
+
+    return env
+  }
+}
---
+++ cell-layer/control-plane/service/src/test/groovy/simplepaas/controlplane/CreateImageSpec.groovy
@@ -0,0 +1,98 @@
+package simplepaas.controlplane
+
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+import simplepaas.controlplane.commands.CreateContainer
+import spock.lang.Specification
+
+class CreateImageSpec extends Specification {
+
+  JSONApi api = Mock(JSONApi)
+
+  def "Correctly passes host and networking settings for sp_proxy"() {
+    given:
+    def command = new CreateContainer(dockerApi: api)
+    def jsonRequest = [
+            imageId: "sp_proxy",
+            name: "sp_proxy",
+    ]
+
+    when:
+    command(jsonRequest)
+
+    then:
+    1 * api.post("/containers/create?fromImage=sp_proxy&name=sp_proxy", {
+      def json = new JsonSlurper().parseText(it)
+    }) >> [Id: "abcdef123456"]
+
+    1 * api.post("/containers/abcdef123456/start", {
+      def json = new JsonSlurper().parseText(it)
+      json.PortBindings."8888/tcp"[0].HostPort == "8888"
+    }) >> [Id: "abcdef123456"]
+  }
+
+  def "Correctly calls the docker API"() {
+    given:
+    def command = new CreateContainer(dockerApi: api)
+    def jsonRequest = [
+            imageId: "123456",
+            name: "awesome",
+            dependencies: [
+                    [
+                            dependency: "postgres",
+                            host: "172.40.1.3",
+                            port: 746578
+                    ],
+                    [
+                            dependency: "wibbleApi",
+                            host: "172.40.1.7",
+                            port: 66666
+                    ]
+            ]
+    ]
+
+    when:
+    command(jsonRequest)
+
+    then:
+    1 * api.post("/containers/create?fromImage=123456&name=awesome", {
+      def json = new JsonSlurper().parseText(it)
+      json.Env.find { it.contains("postgres") } != null
+    }) >> [Id: "THEIDOFDOOM"]
+  }
+
+  def "Correctly interprets the dependency info to build ENV"() {
+    given:
+    def command = new CreateContainer(dockerApi: api)
+
+    def jsonResponse = [
+            containerId: "789456",
+            dependencies: [
+                    [
+                            dependency: "postgres",
+                            host: "172.40.1.3",
+                            port: 746578
+                    ],
+                    [
+                            dependency: "wibbleApi",
+                            host: "172.40.1.7",
+                            port: 66666
+                    ]
+            ]
+    ]
+
+    when:
+    def env = command.getEnvironmentVariables(jsonResponse)
+
+    then:
+    env.size() == 4
+    env[0] == "postgres_PORT=746578"
+    env[1] == "postgres_HOST=172.40.1.3"
+    env[2] == "wibbleApi_PORT=66666"
+    env[3] == "wibbleApi_HOST=172.40.1.7"
+  }
+
+  def json(map) {
+    new JsonBuilder(map).toString()
+  }
+}
