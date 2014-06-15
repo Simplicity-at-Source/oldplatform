662facb465e5d491f714a366632cd11d892d95cf
Gawain <spam.gawain@gmail.com>
2014-06-15 09:54:42 -0700

    got muon tests working with nucleus

--- test-scripts/create-container.sh
+++ test-scripts/create-container.sh
@@ -2,11 +2,12 @@
 
 
 
-curl -vvv -X POST http://172.17.0.4:8080/service/gene-store/substore/cell -d '{ "id": "simplenode2", "image": "sp_platform/uber-any", "env": { "GIT_REPO_URL": "https://github.com/fuzzy-logic/simplenode.git", "DNSHOST": "simplenode.muon.cistechfutures.net" } }' -H 'Content-Type: application/json'
+
+curl -vvv -X PUT http://172.17.0.4:8080/service/gene-store/substore/cell/record/simplenode -d @create-container.json -H 'Content-Type: application/json'
 
 
-#sleep 10
 
-#IPADDR=`docker inspect 5cf8be02bd5b | grep -i IPAddress | awk -F\" '{print $4}'`
 
-#curl -vvv -X GET http://$IPADDR:8080/
+
+
+
