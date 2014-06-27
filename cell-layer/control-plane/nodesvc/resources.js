d694dafbdb55b49bb080d6d5b612cafd15f87439
fuzzy-logic <spam.gawain@gmail.com>
2014-06-27 17:19:31 +0100

    control-plane wworkign with msh url templates

--- cell-layer/control-plane/nodesvc/resources.js
+++ cell-layer/control-plane/nodesvc/resources.js
@@ -130,7 +130,7 @@ exports.postContainer = {
   },
   'action': function (req,res) {
     
-    console.log('resources.js postContainer() req.body=' + JSON.stringify(req.body));
+    console.log('\nresources.js postContainer() req.body=' + JSON.stringify(req.body));
     var payload = req.body;
 
       if (! payload) {
@@ -144,46 +144,113 @@ exports.postContainer = {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 function createAndStartDockerContainer(req, res, payload)  {
-     
+
+      //console.log('resources.js postContainer() imageUrl=' + imageUrl);
+      //console.log('resources.js postContainer() containerUrl=' + containerUrl);
+
+     var errCallback = function(error) {
+          sendServerError(res, {}, [], 'unknown error ' + err , 0);
+      }
+
+    
+        var stopPredicate = function() {
+              if (nucleusUp) { return false; }
+                return true;
+        }
+
+        var urlTemplate = function(lastHttpAction, queue) {
+            var template =  {
+                dockerId: queue[0].response.Id,
+            };
+            //console.log("urlTemplate: " + JSON.stringify(template));
+            return template;
+        }
+        
+        var geneToDockerTransformer = function(dockerStartJson) {
+             var result =  createDockerStartJson(dockerStartJson, payload);
+            return result;
+        }
+
+      var callback = function(actions) {
+          //console.log('resources.js postContainer()->callback()');
+          
+          if (actions[0].statusCode != '201') {
+              sendServerError(res, actions[0], actions, 'error creating new container via docker api'  , 201);  
+              return;
+          } 
+          //else if (! actions.allOk() ) {
+        //      res.send(500, {message: 'error during msh callbacks', logs: actions});    
+        //  }
+          
+          if (actions[3] && actions[3].statusCode != '204') {
+              sendServerError(res, actions[3], actions, 'while starting docker container with id  ' +  actions[0].response.Id, 204);  
+              return;
+          } 
+          if (actions[5] && actions[5].statusCode != '200') {
+               sendServerError(res, actions[5], actions, 'while gettting started docker container json', 200 );
+              return;
+          }
+          if (actions[9] && actions[9].statusCode != '201') {
+               //console.log("********** WARNING: PROBLEM ATTEMPTING TO PUT TO NUCLEUS action=" + JSON.stringify(actions[3]));
+          } 
+              
+                  logCoreServices(actions[5].response);
+                res.send(201, {message: 'Container created', id: actions[5].response.Id}); 
+          
+      }
+
+      var dockerPayload = injectPlatformVariables(transformer.muonToDocker(payload));
 
       var imageUrl = '/images/create?fromImage=' + payload.imageId;
       var containerUrl = '/containers/create?name=' + payload.name;
       
-      //console.log('resources.js postContainer() imageUrl=' + imageUrl);
-      //console.log('resources.js postContainer() containerUrl=' + containerUrl);
-
-      var callback = function(actions) {
-          //console.log('resources.js postContainer()->callback()');
-          // after we've created the docker container and get it's ID, we can start the container
-
-          //if (actions[0].statusCode != '201') {
-        //         sendServerError(res, actions[0], actions, 'error creating new image via docker api'  );    
-        //  } 
-          if (actions[0].statusCode != '201') {
-              sendServerError(res, actions[0], actions, 'error creating new container via docker api'  , 201);    
-          } 
-          else if (! actions.allOk() ) {
-              res.send(500, {message: 'error during msh callbacks', logs: actions});    
-          }
-          var dockerReply =  actions[0].response;
-          //console.log('resources.js postContainer() dockerReply=' + dockerReply);
-          
-          startContainer(req, res, dockerReply, payload);
-      }
-      
-      var errCallback = function(error) {
-          sendServerError(res, {}, [], 'unknown error ' + err , 0);
-      }
-      
-      var dockerPayload = transformer.muonToDocker(payload);
-      //console.log('resources.js postContainer() msh.init() dockerPayload=' + JSON.stringify(dockerPayload));
-      
       msh.init(callback, errCallback)
-      //.post(dockerIp, dockerPort, imageUrl, {})
-      .post(dockerIp, dockerPort, containerUrl, injectPlatformVariables(dockerPayload))
+      //.post(dockerIp, dockerPort, imageUrl, {}) // not currently cerating the image
+      .post(dockerIp, dockerPort, containerUrl, dockerPayload)
+      .template(urlTemplate) 
+      .pipe()
+      .post(dockerIp, dockerPort, '/containers/{dockerId}/start')
+      .template(urlTemplate) 
+      .get(dockerIp, dockerPort, '/containers/{dockerId}/json')
+      .stop(stopPredicate)
+      .pipe(createPokemonJson)
+      .template(urlTemplate) 
+      .put(nucleusHost, nucleusPort, pokemonPath + '/record/{dockerId}')
       .end();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -198,78 +265,13 @@ function sendServerError(res, action, actions, message, expectedCode) {
     res.send(500, {message: message, error: 'request error in action ' + action.counter, action: JSON.stringify(action) });   
 }
 
-function startContainer(req, res, dockerReply, payload)  {
-    
-    
-     //if (payload.name.indexOf('nucleus') > -1) {
-     //         console.log("******************** nucleus container created, setting nucleusUp=true");
-      //        nucleusUp = true;
-       //   }
-    
-     var dockerStartJson =  createDockerStartJson(dockerStartJson, payload);
-
-      var startUrl = '/containers/' + dockerReply.Id + '/start';
-
-      var callback = function(actions) {
-         console.log('*********************************************************************************');
-           console.log('resources.js postContainer()->callback() actions:');
-           //console.log('payload=' + JSON.stringify(payload));
-          // console.log('dockerStartJson=' + JSON.stringify(dockerStartJson));
-          console.dir(actions);
-          //console.log("actions[0].statusCode=" + actions[0].statusCode);
-          
-          
-          
-          if (actions[0] && actions[0].statusCode != '204') {
-              sendServerError(res, actions[0], actions, 'while starting docker container with id  ' +  dockerReply.Id, 204);  
-              return;
-          } 
-          if (actions[1] && actions[1].statusCode != '200') {
-               sendServerError(res, actions[1], actions, 'while gettting started docker container json', 200 );
-              return;
-          }
-          if (actions[3] && actions[3].statusCode != '201') {
-               console.log("********** WARNING: PROBLEM ATTEMPTING TO PUT TO NUCLEUS action=" + JSON.stringify(actions[3]));
-              res.send(201, {message: 'Container created', id: dockerReply.Id});
-              return;
-          }
-          
-        logCoreServices(actions[1].response);
-          
-          res.send(201, {message: 'Container created', id: dockerReply.Id});   
-          
-          
-      }
-
-      var errCallback = function(err) {
-           //console.log('resources.js postContainer()->callback()->innerErrCallback()');
-          sendServerError(res, {}, [], 'unknown error ' + err , 0);
-      }
-      
-      
-      if (nucleusUp) {
-            msh.init(callback, errCallback)
-           .post(dockerIp, dockerPort, startUrl, dockerStartJson)
-           .get(dockerIp, dockerPort, '/containers/' + dockerReply.Id + '/json')
-           .pipe(createPokemonJson)
-           .put(nucleusHost, nucleusPort, pokemonPath + '/record/' + dockerReply.Id)
-           .end();
-      } else {
-            console.log("********** Nucleus Down Not PUTing container info to nucleus **********");
-            msh.init(callback, errCallback)
-           .post(dockerIp, dockerPort, startUrl, dockerStartJson)
-            .get(dockerIp, dockerPort, '/containers/' + dockerReply.Id + '/json')
-           .end();
-      }
-
-}
 
 
 function logCoreServices(dockerPayload) {
     
-     console.log('********************************************************** resources.js logCoreServices() ****************************************************************************************');
-    console.log('resources.js logCoreServices() dockerPayload=' + JSON.stringify(dockerPayload));
-     console.log('resources.js logCoreServices() coreServices=' + JSON.stringify(coreServices));
+    // console.log('*************** resources.js logCoreServices() ****************');
+    //console.log('resources.js logCoreServices() dockerPayload=' + JSON.stringify(dockerPayload));
+    // console.log('resources.js logCoreServices() coreServices=' + JSON.stringify(coreServices));
     var host;
     var port;
     var service = undefined;
@@ -284,7 +286,7 @@ function logCoreServices(dockerPayload) {
       } 
     
     if (service) {
-         console.log("******************** %s core service container created. Setting up coreServices object with host/port data", service); 
+        // console.log("******************** %s core service container created. Setting up coreServices object with host/port data", service); 
          coreServices[service] = {};
         coreServices[service].id = dockerPayload.Id;
         coreServices[service].service = service;
@@ -299,22 +301,22 @@ function logCoreServices(dockerPayload) {
     
 }
 function injectPlatformVariables(dockerPayload) {
-   console.log('********************************************************** resources.js injectPlatformVariables() ****************************************************************************************');
-    console.log('resources.js injectPlatformVariables() dockerPayload=' + JSON.stringify(dockerPayload));
-     console.log('resources.js injectPlatformVariables() coreServices=' + JSON.stringify(coreServices));
+  // console.log('********** resources.js injectPlatformVariables() *******************');
+  //  console.log('resources.js injectPlatformVariables() dockerPayload=' + JSON.stringify(dockerPayload));
+  //   console.log('resources.js injectPlatformVariables() coreServices=' + JSON.stringify(coreServices));
     
     if (coreServices.nucleus) {
-        console.log('******************** injectPlatformVariables() enriching with nucleus data');  
+       // console.log('********** injectPlatformVariables() enriching with nucleus data');  
        dockerPayload.Env.push("MUON_NUCLEUS_IP=" + coreServices.nucleus.host);
        dockerPayload.Env.push("MUON_NUCLEUS_PORT=" + coreServices.nucleus.port);
     }
     if (coreServices.gns) {
-         console.log('******************** injectPlatformVariables() enriching with gns data');  
+       //  console.log('********** injectPlatformVariables() enriching with gns data');  
        dockerPayload.Env.push("MUON_GNS_IP=" + coreServices.gns.host);
        dockerPayload.Env.push("MUON_GNS_PORT=" + coreServices.gns.port);
     }
     if (coreServices.proxy) {
-         console.log('******************** injectPlatformVariables() enriching with proxy data');  
+         console.log('********** injectPlatformVariables() enriching with proxy data');  
        dockerPayload.Env.push("MUON_GNS_IP=" + coreServices.proxy.host);
        dockerPayload.Env.push("MUON_GNS_PORT=" + coreServices.proxy.port);
     }   
@@ -323,12 +325,12 @@ function injectPlatformVariables(dockerPayload) {
     dockerPayload.Env.push("MUON_DOMAIN=" + muonDomain);
     
       
-     console.log('******************** resources.js END injectPlatformVariables() dockerPayload=' + JSON.stringify(dockerPayload));
+    // console.log('********** resources.js END injectPlatformVariables() dockerPayload=' + JSON.stringify(dockerPayload));
     return dockerPayload;
 }
 
 var  createPokemonJson = function(dockerJson) {
-    console.log("transforming docker response to nucleus payload");
+    //console.log("transforming docker response to nucleus payload");
     var id = dockerJson.Id
     /*
      it.id = it.Id
